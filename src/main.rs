0f937564c2618a45a4a9ee34371eef6f2850a95e
Masked <trist13579@gmail.com>
2024-01-14 23:17:01 -0500

    Built out income calcs and UI

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+use slint::SharedString;
+
 slint::include_modules!();
+
+const TAXPER: f64 = 0.30;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
     let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
+    ui.on_divide_income(move |string: SharedString| {
         let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+        let num: f64 = string.trim().parse().unwrap();
+        let tax: f64 = num * TAXPER;
+        let owner: f64 = num * OWNERPER;
+        let profit: f64 = num * PROFITPER;
+        let opex: f64 = num * OPEXPER;
+        let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOperating Expense: {:.2}\n", {tax}, {owner}, {profit}, {opex});
+        ui.set_results(result.into());
     });
 
     ui.run()
